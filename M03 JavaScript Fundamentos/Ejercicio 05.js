66e8a16da3473cd3e308fd1c7d0acf4033d47154
Ramiro <rama_laacade@hotmail.com>
2023-11-21 21:33:18 -0300

    segundoHomework

--- M03 JavaScript Fundamentos/Ejercicio 05.js
+++ M03 JavaScript Fundamentos/Ejercicio 05.js
@@ -20,7 +20,7 @@ function agregarSimboloExclamacion(str) {
    // Agrega un símbolo de exclamación al final del string "str" y retórnalo
    // Ejemplo: "hello world" ---> "hello world!"
    // Tu código:
-   return (str + ! )
+   return 'str' + ! 
 }
 
 function combinarNombres(nombre, apellido) {
